0069f52fa8e830228471391da21d2f9cde85920c
Pedro Fornazari <71901236+PedroFornazari@users.noreply.github.com>
2022-08-18 21:52:05 -0300

    Update Exercicio2_Distancia_E_Tempo.py

--- Exercicio2_Distancia_E_Tempo.py
+++ Exercicio2_Distancia_E_Tempo.py
@@ -2,7 +2,7 @@ distancia = 0
 tempo = 0
 velocidade_media = 0
 
-print('EXERCICIO 3:')
+print('EXERCICIO 2:')
 distancia = int(input('Digite a distância em Km que deseja percorrer: '))
 velocidade_media = int(input('Digite qual a velocidade média do carro na viagem: '))
 
