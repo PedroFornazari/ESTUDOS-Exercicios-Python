5cd381a37df73267d394901ba0cebf86ff420dd3
Pedro Fornazari <71901236+PedroFornazari@users.noreply.github.com>
2022-08-18 21:23:54 -0300

    Add files via upload

---
+++ Exercicio1_Distancia_E_Velocidade.py
@@ -0,0 +1,11 @@
+distancia = 0
+tempo = 0
+velocidade_media = 0
+
+print('EXERCICIO 2:')
+distancia = int(input('Digite a distância em Km que deseja percorrer: '))
+tempo = int(input('Digite em quanto tempo você quer chegar em horas: '))
+
+velocidade_media = distancia / tempo
+
+print('Velocidade Média necessária: ', velocidade_media, 'Km/h')
