eff52cc8afb64f9fc6836531f0d13541665d37cd
Pedro Fornazari <71901236+PedroFornazari@users.noreply.github.com>
2022-08-25 20:49:11 -0300

    Add files via upload

---
+++ Exercicio3_VolumeCubo.py
@@ -0,0 +1,15 @@
+comprimento = 0
+largura = 0
+altura = 0
+volume = 0
+
+print('EXERCICIO 3')
+print('CÁLCULO DE UM VOLUME DE UM CUBO')
+
+comprimento = int(input('Valor do Comprimento do cubo: '))
+largura = int(input('Valor da Largura do cubo: '))
+altura = int(input('Valor da Altura do cubo: '))
+
+volume = comprimento * altura * largura
+
+print('VOLUME DO CUBO: ', volume)
