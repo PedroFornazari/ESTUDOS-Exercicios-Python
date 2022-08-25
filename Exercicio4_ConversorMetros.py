cd2fd19b9648b4a1d72d599f891a36872aa7217b
Pedro Fornazari <71901236+PedroFornazari@users.noreply.github.com>
2022-08-25 20:54:54 -0300

    Add files via upload

---
+++ Exercicio4_ConversorMetros.py
@@ -0,0 +1,11 @@
+metros = 0
+centimetros = 0
+
+print('EXERCICIO 3')
+print('CONVERSOR DE METROS PARA CENTRÍMETROS: ')
+
+metros = float(input('VALOR EM METROS: '))
+
+centimetros = metros * 100
+
+print('VALOR EM CENTÍMETROS: ', centimetros)
